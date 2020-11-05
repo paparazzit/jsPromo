0af106d90a45be3a0b49396a39ef4c5da9e396c0
unknown <paparazzit@gmail.com>
2020-11-05 17:00:59 +0100

    Js promo

---
+++ main.js
@@ -0,0 +1,13 @@
+const naslov = "Full digitalna usluga";
+let i = 0;
+
+const kuckanje = () => {
+  if (i < naslov.length) {
+    document.querySelector(".tipkajuciNaslov").innerHTML += naslov.charAt(i);
+    i++;
+
+    setTimeout(kuckanje, 150);
+  }
+};
+
+kuckanje();
